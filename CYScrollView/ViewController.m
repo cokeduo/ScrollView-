8d4663155b3c5ad7f3e974748a829b802e61d116
王铎 <cokeduo@163.com>
2016-03-01 14:44:18 +0800

    首页广告位图片的循环滚动播放

--- CYScrollView/ViewController.m
+++ CYScrollView/ViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+WebCache.h"
 
-@interface ViewController ()
+#define KWidth self.view.bounds.size.width
+#define KSliderHeight 142
+
+@interface ViewController ()<UIScrollViewDelegate>
+
+@property (nonatomic,strong) UIScrollView *scrollView;
+@property (nonatomic,strong) UIPageControl *pageControl;
+@property (nonatomic,strong) NSTimer *timer;
+@property (nonatomic,assign) NSInteger pageIndex;
+@property (nonatomic,assign) NSInteger pageCount;
 
 @end
 
@@ -16,12 +26,111 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    NSArray *imagesArr = [NSArray arrayWithObjects:@"http://182.92.101.144:8080/ibiaoqing/admin/advertisement/download.do?aId=1",@"http://182.92.101.144:8080/ibiaoqing/admin/advertisement/download.do?aId=2",@"http://182.92.101.144:8080/ibiaoqing/admin/advertisement/download.do?aId=3", nil];
+    
+    NSArray *imagesArr = [NSArray arrayWithObjects:[UIImage imageNamed:@"image_1"],[UIImage imageNamed:@"image_2"],[UIImage imageNamed:@"image_3"], nil];
+    
+    NSMutableArray *newImageArr = [[NSMutableArray alloc] initWithArray:imagesArr];
+    [newImageArr insertObject:[imagesArr lastObject] atIndex:0];
+    [newImageArr addObject:[imagesArr firstObject]];
+    
+    self.pageIndex = 0;
+    self.pageCount = imagesArr.count;
+    
+
+    UIScrollView *tempScroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, KWidth, KSliderHeight)];
+    tempScroll.contentSize = CGSizeMake(KWidth*newImageArr.count, KSliderHeight);
+    tempScroll.pagingEnabled = YES;
+    tempScroll.delegate = self;
+    tempScroll.bounces = NO;
+    tempScroll.showsHorizontalScrollIndicator = NO;
+    tempScroll.contentOffset = CGPointMake(KWidth, 0);
+    self.scrollView = tempScroll;
+    [self.view addSubview:self.scrollView];
+    
+    
+    for (int i = 0; i < newImageArr.count; i++) {
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(KWidth*i, 0, KWidth, KSliderHeight)];
+//        [imageView sd_setImageWithURL:[NSURL URLWithString:[newImageArr objectAtIndex:i]] placeholderImage:nil];
+        imageView.image = [newImageArr objectAtIndex:i];
+        imageView.userInteractionEnabled = YES;
+        [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickedImageViewAtIndex:)]];
+        [self.scrollView addSubview:imageView];
+    }
+    
+    
+    UIPageControl *tempPageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, KSliderHeight-20, KWidth, 20)];
+    tempPageControl.numberOfPages = self.pageCount;
+    tempPageControl.currentPage = self.pageIndex;
+    tempPageControl.pageIndicatorTintColor = [UIColor whiteColor];
+    tempPageControl.currentPageIndicatorTintColor = [UIColor purpleColor];
+    self.pageControl = tempPageControl;
+    [self.view addSubview:self.pageControl];
+    
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(beginPlayImage) userInfo:nil repeats:YES];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - timer 事件
+-(void)beginPlayImage{
+    
+    self.pageIndex++;
+    if (self.pageIndex == self.pageCount) {
+        self.pageIndex = 0;
+    }
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.scrollView.contentOffset = CGPointMake(self.scrollView.contentOffset.x + KWidth, 0);
+    } completion:^(BOOL finished) {
+        self.pageControl.currentPage = self.pageIndex;
+    }];
+    
+    
+    int index = self.scrollView.contentOffset.x/KWidth;
+    if (index == 0) {
+        self.scrollView.contentOffset = CGPointMake(KWidth*self.pageCount, 0);
+        self.pageIndex = self.pageCount - 1;
+    }else if (index == self.pageCount + 1){
+        self.scrollView.contentOffset = CGPointMake(KWidth, 0);
+        self.pageIndex = 0;
+    }else{
+        self.pageIndex = index - 1;
+    }
+
+    
 }
+
+#pragma mark - scrollView delegate
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    
+    int index = self.scrollView.contentOffset.x/KWidth;
+    if (index == 0) {
+        self.scrollView.contentOffset = CGPointMake(KWidth*self.pageCount, 0);
+        self.pageIndex = self.pageCount - 1;
+    }else if (index == self.pageCount + 1){
+        self.scrollView.contentOffset = CGPointMake(KWidth, 0);
+        self.pageIndex = 0;
+    }else{
+        self.pageIndex = index - 1;
+    }
+    
+    self.pageControl.currentPage = self.pageIndex;
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(beginPlayImage) userInfo:nil repeats:YES];
+    
+}
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    [self.timer invalidate];
+}
+
+#pragma mark - imageView 点击事件
+-(void)clickedImageViewAtIndex:(UITapGestureRecognizer *)tapGesture{
+    NSLog(@"pageIndex:%ld",self.pageIndex);
+}
+
 
 @end
